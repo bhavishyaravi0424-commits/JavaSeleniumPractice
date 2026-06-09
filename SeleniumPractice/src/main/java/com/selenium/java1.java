a7b613935ae84fec7d753897493aea79ea0575b0
Bhavishya <bhavishyaravi0424@gmail.com>
2026-06-09 20:08:14 +0530

    basic java program

---
+++ SeleniumPractice/src/main/java/com/selenium/java1.java
@@ -0,0 +1,16 @@
+package com.selenium;
+
+public class java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num = 43;
+      String website = "Bhavishya";
+      char letter ='r';
+      double marks =5.6;
+      boolean myCard = true;
+      System.out.println(num + "is the value stored in the computer");
+
+	}
+
+}
